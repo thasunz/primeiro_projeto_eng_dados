3dd328b3959b630c2ee5f4ef11a9df28579ec6f8
thales <tbfelix00@outlook.com>
2025-02-20 20:24:06 -0300

    adicionando de volta o arquivo

---
+++ tests/main.py
@@ -0,0 +1,21 @@
+print ("Jornada de dados")
+
+#Variáveis 
+
+idade = 34
+nome = "Thales"
+
+# Print
+
+print(idade)
+
+
+# Soma
+nova_idade = idade + 1
+
+#Lista
+
+lista = [1,2,3,4,5]
+
+#Métodos
+lista_invertidade = lista.reverse()
